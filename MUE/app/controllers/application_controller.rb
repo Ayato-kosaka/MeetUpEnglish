8acfe5de2f6e22e21dfecd2bd681a69932994570
Ayato-kosaka <kosaka.ayato@gmail.com>
2019-08-05 21:01:15 +0900

    first commit

--- MUE/app/controllers/application_controller.rb
+++ MUE/app/controllers/application_controller.rb
@@ -4,9 +4,4 @@ class ApplicationController < ActionController::Base
   private
 
   def require_Admin
-    unless session[:role] == "Admin"
-      flash[:alert] = "You must be logged in to access this section"
-      redirect_to login_url # halts request cycle
-    end
-  end
 end
