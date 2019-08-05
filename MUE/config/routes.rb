6f9be876418d726017765275fa98fdab3ea02929
Ayato-kosaka <kosaka.ayato@gmail.com>
2019-08-06 01:44:41 +0900

    first commit

--- MUE/config/routes.rb
+++ MUE/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  get 'home/index'
   resources :caves
   resources :regions
   resources :homes
+  root 'homes#home'
   resources :prefectures
   resources :cities
   controller :sessions do
