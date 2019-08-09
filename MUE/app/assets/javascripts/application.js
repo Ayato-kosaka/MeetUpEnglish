b6fc5e5086a0a2136920f599e8d7a12ad2a12db3
Ayato-kosaka <kosaka.ayato@gmail.com>
2019-08-09 20:04:57 +0900

    first commit

--- MUE/app/assets/javascripts/application.js
+++ MUE/app/assets/javascripts/application.js
@@ -16,17 +16,15 @@
 //= require turbolinks
 //= require_tree .
 $(function(){
-  setTimeout(function(){
 
-  $(".region_a").parent().children("ul").hide();
-  $("#display_show").show();//のちに消す
-  $(".region_a").click(function(){
-    var region_childern_ul = $(this).parent().children("ul")
-    if(region_childern_ul.is(':hidden')){
-      region_childern_ul.show();
-    } else{
-      region_childern_ul.hide();
-    }
-  });
-},100);
+$(".region_a").parent().children("ul").hide();
+$("#display_show").show();//のちに消す
+$(".region_a").click(function(){
+  var region_childern_ul = $(this).parent().children("ul")
+  if(region_childern_ul.is(':hidden')){
+    region_childern_ul.show();
+  } else{
+    region_childern_ul.hide();
+  }
+ });
 })
