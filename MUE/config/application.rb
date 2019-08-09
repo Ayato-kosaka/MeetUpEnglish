24da7eb1aa9054fc8c3d1b8438783fcbdb2448e9
Ayato-kosaka <kosaka.ayato@gmail.com>
2019-08-10 07:19:36 +0900

    first commit

--- MUE/config/application.rb
+++ MUE/config/application.rb
@@ -10,6 +10,10 @@ module MUE
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # 表示時のタイムゾーンをJSTに設定
+    config.time_zone = 'Tokyo'
+    # DB保存時のタイムゾーンをJSTに設定
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
