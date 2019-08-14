620b783d1ac23b043daf663c7c351f7a1bd873bf
Ayato-kosaka <kosaka.ayato@gmail.com>
2019-08-15 03:29:17 +0900

    first commit

--- MUE/app/controllers/cities_controller.rb
+++ MUE/app/controllers/cities_controller.rb
@@ -75,6 +75,6 @@ class CitiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def city_params
-      params.require(:city).permit(:name, :prefectureId, images: [])
+      params.require(:city).permit(:name, :prefectureId)
     end
 end
