71bb42dc77ad423ce8ebca649c7f37eb79e60c60
Ayato-kosaka <kosaka.ayato@gmail.com>
2019-09-15 13:40:32 +0900

    first commit

--- MUE/app/helpers/application_helper.rb
+++ MUE/app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  def default_meta_tags
+  {
+    site: 'Meet Up English',
+    reverse: true,
+    separator: '|',
+    og: defalut_og,
+    twitter: default_twitter_card
+  }
+end
+
+private
+
+def defalut_og
+  {
+    title: :full_title,          # :full_title とすると、サイトに表示される <title> と全く同じものを表示できる
+    description: :description,   # 上に同じ
+    url: request.url,
+    image: 'https://firebasestorage.googleapis.com/v0/b/test-8d0bf.appspot.com/o/images%2FS__5120002.jpg?alt=media&token=0d0a3277-931a-4822-a88c-9bed623c0c19'
+  }
+end
+
+def default_twitter_card
+  {
+    card: 'summary_large_image', # または summary
+    site: '@meetup_english'            # twitter ID
+  }
+end
 end
