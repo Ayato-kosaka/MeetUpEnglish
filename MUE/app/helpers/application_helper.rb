6256c0a98f0a67ea6e97a5ba834ef35059b3abf5
Ayato-kosaka <kosaka.ayato@gmail.com>
2019-09-23 01:44:33 +0900

    a

--- MUE/app/helpers/application_helper.rb
+++ MUE/app/helpers/application_helper.rb
@@ -9,21 +9,21 @@ module ApplicationHelper
   }
 end
 
-private
+  private
 
-def defalut_og
-  {
-    title: :full_title,          # :full_title とすると、サイトに表示される <title> と全く同じものを表示できる
-    description: :description,   # 上に同じ
-    url: request.url,
-    image: 'https://firebasestorage.googleapis.com/v0/b/test-8d0bf.appspot.com/o/images%2FS__5120002.jpg?alt=media&token=0d0a3277-931a-4822-a88c-9bed623c0c19'
-  }
-end
+    def defalut_og
+      {
+        title: :full_title,          # :full_title とすると、サイトに表示される <title> と全く同じものを表示できる
+        description: :description,   # 上に同じ
+        url: request.url,
+        image: 'https://firebasestorage.googleapis.com/v0/b/test-8d0bf.appspot.com/o/images%2FS__5120002.jpg?alt=media&token=0d0a3277-931a-4822-a88c-9bed623c0c19'
+      }
+    end
 
-def default_twitter_card
-  {
-    card: 'summary_large_image', # または summary
-    site: '@meetup_english'            # twitter ID
-  }
-end
+    def default_twitter_card
+      {
+        card: 'summary_large_image', # または summary
+        site: '@meetup_english'            # twitter ID
+      }
+    end
 end
