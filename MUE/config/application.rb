a7079346827f4d36e17b11d3cef6c84a3310081a
Ayato-kosaka <kosaka.ayato@gmail.com>
2019-10-09 05:49:50 +0900

    fix error div to span

--- MUE/config/application.rb
+++ MUE/config/application.rb
@@ -15,6 +15,9 @@ module MUE
     # DB保存時のタイムゾーンをJSTに設定
     config.active_record.default_timezone = :local
 
+    # change div field_with_error to span span_with_error
+    config.action_view.field_error_proc =
+      Proc.new { |html_tag, instance| %Q(<span class="span_with_error">#{html_tag}</span>).html_safe }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
