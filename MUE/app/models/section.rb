482222aa913074b4711c8c1571b111d95f1b1e62
Ayato-kosaka <kosaka.ayato@gmail.com>
2019-10-21 18:00:55 +0900

    bugg fix

--- MUE/app/models/section.rb
+++ MUE/app/models/section.rb
@@ -7,7 +7,7 @@ class Section < ApplicationRecord
       this_blog = section.blog
       last_blog = Blog.where(blogcategory_id: category_id).where('created_at<?', this_blog.created_at).last
       a_href = "href='/blogs/#{last_blog.id}'"
-      section.text += ("<div id='last_lesson'><a class='image' #{a_href}><img src='#{ this_blog.image.attachment.service.send(:object_for, this_blog.image.key).public_url }'></a><a class='title' #{a_href}>#{ last_blog.title }</a></div>")
+      section.text += ("<div id='last_lesson'><a class='image' #{a_href}><img src='#{ last_blog.image.attachment.service.send(:object_for, last_blog.image.key).public_url }'></a><a class='title' #{a_href}>#{ last_blog.title }</a></div>")
     end
   end
 end
