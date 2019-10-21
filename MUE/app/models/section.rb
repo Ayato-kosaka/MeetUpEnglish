f4ce3ba1a91aa8c9f019d8cbd5ed46fed10ce66a
Ayato-kosaka <kosaka.ayato@gmail.com>
2019-10-21 17:29:30 +0900

    bug fix

--- MUE/app/models/section.rb
+++ MUE/app/models/section.rb
@@ -3,7 +3,7 @@ class Section < ApplicationRecord
   belongs_to :blog
   after_validation do |section|
     if section.title == "前回の開催の様子"
-      category_id = 8
+      category_id = 4
       this_blog = section.blog
       last_blog = Blog.where(blogcategory_id: category_id).where('created_at<?', this_blog.created_at).last
       a_href = "href='/blogs/#{last_blog.id}'"
