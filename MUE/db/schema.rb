67ed37df1255210f61863d486e6092f0a0887bee
Ayato-kosaka <kosaka.ayato@gmail.com>
2019-10-29 22:50:31 +0900

    merge

--- MUE/db/schema.rb
+++ MUE/db/schema.rb
@@ -48,11 +48,13 @@ ActiveRecord::Schema.define(version: 2019_10_21_084250) do
   end
 
   create_table "blogcomments", force: :cascade do |t|
+    t.integer "blog_id"
     t.string "name"
     t.text "text"
-    t.integer "blog_id"
+    t.integer "test"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_blogcomments_on_blog_id"
   end
 
   create_table "blogs", force: :cascade do |t|
