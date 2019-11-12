ffb7125ce3cb5f9997260322f0168192f426f1e3
Ayato-kosaka <kosaka.ayato@gmail.com>
2019-11-12 14:03:11 +0900

    algolism of creating place

--- MUE/app/controllers/teacher/other_controller.rb
+++ MUE/app/controllers/teacher/other_controller.rb
@@ -3,26 +3,42 @@ class Teacher::OtherController < TeacherController
   require 'net/https'
   require 'json'
 
+  #teacher_newPlace_other_path 	GET 	/teacher/new_my_place(.:format) 	teacher/other#newPlace
   def newPlace
   end
 
+  #teacher_createPlace_other_path 	POST 	/teacher/create_my_place(.:format) 	teacher/other#createPlace
   def createPlace #外国の対策が未実施
     place = Place.find_or_initialize_by(placeId: params[:placeId])
+    #placeId 例 ChIJDTc9GIv7GGAR0wyzbYIBuQ8
     if place.new_record?
                                         en_http = Net::HTTP.get(URI.parse("https://maps.googleapis.com/maps/api/place/details/json?place_id=#{placeId}&fields=name&language=en&key=AIzaSyBxj9aDYTbBYilvyaYugd9J5zisCodO9kQ"))
       en_json = JSON.parse(en_http)["result"]
       place.en_name = en_json["name"]
-                                        ja_http = Net::HTTP.get(URI.parse("https://maps.googleapis.com/maps/api/place/details/json?place_id=#{placeId}&fields=name,website,geometry,formatted_address&language=ja&key=AIzaSyBxj9aDYTbBYilvyaYugd9J5zisCodO9kQ"))
+                                        ja_http = Net::HTTP.get(URI.parse("https://maps.googleapis.com/maps/api/place/details/json?place_id=#{placeId}&fields=name,website,geometry,address_component&language=ja&key=AIzaSyBxj9aDYTbBYilvyaYugd9J5zisCodO9kQ"))
       ja_json = JSON.parse(ja_http)["result"]
       place.ja_name = ja_json["name"]
       place.website = ja_json["website"]
       location = ja_json["geometry"]["location"]
       place.latitude = location["lat"]
       place.longitude = location["lng"]
-      formatted_address = ja_json["formatted_address"]
-      place.save#if文で分けたいところ
+      address = ja_json["address_components"]
+      address = address.reverse.map{|n|n["long_name"]}
+      if address[1] == "日本"
+        prefecture = Prefecture.find_by(name: address[2])
+        (3...address.length).each do |num|
+          str = address[3..num].join
+          break if city = prefecture.cities.find_by(name: str)
+        end
+        place.city_id = city.id #city_idをつくらないといけない
+        unless place.save
+          flash.now[:alert] = place.errors
+          render :newPlace
+        end
+      else
+        redirect_to teacher_newPlace_other_path
+      end
     end
-
     # redirect_to teacher_schedule_index_path if @current_teacher.teacher_places.create(place_id: place.id)
   end
 end
--- MUE/app/controllers/users/search_controller.rb
+++ MUE/app/controllers/users/search_controller.rb
@@ -1,10 +1,25 @@
-class Users::SearchController < ApplicationController
+class Users::SearchController < UsersController
+  before_action :at_search
+
+  #search_path 	GET 	/search(.:format) 	users/search#index
   def index
+    if session[:prefecture].nil?
+      redirect_to search_prefecture_path
+    end
   end
 
+  #search_prefecture_path 	GET 	/search/prefecture(.:format) 	users/search#prefecture
   def prefecture
+    @prefectures = [["北海道", 1], ["東北", 2], ["関東", 3], ["中部", 4], ["近畿", 5], ["中国", 6], ["四国", 7], ["九州・沖縄", 8]]
   end
 
+  #search_city_path 	GET 	/search/city(.:format) 	users/search#city
   def city
   end
+
+
+  private
+    def at_search
+      @at = "search"
+    end
 end
