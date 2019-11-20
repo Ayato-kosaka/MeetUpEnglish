9528f085445d49414ce32050ca5e559d54d70269
Ayato-kosaka <kosaka.ayato@gmail.com>
2019-11-20 21:05:47 +0900

    bugg fix

--- MUE/app/controllers/cities_controller.rb
+++ MUE/app/controllers/cities_controller.rb
@@ -75,6 +75,6 @@ class CitiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def city_params
-      params.require(:city).permit(:name, :prefectureId)
+      params.require(:city).permit(:name, :prefecture_id)
     end
 end
