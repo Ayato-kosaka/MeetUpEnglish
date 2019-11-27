1012c808ad602a08eb631c8fc76a729341ab6c47
Ayato-kosaka <kosaka.ayato@gmail.com>
2019-11-27 17:52:04 +0900

    continous of previous commit

--- MUE/app/helpers/application_helper.rb
+++ MUE/app/helpers/application_helper.rb
@@ -16,7 +16,8 @@ end
         title: :full_title,          # :full_title とすると、サイトに表示される <title> と全く同じものを表示できる
         description: :description,   # 上に同じ
         url: request.url,
-        image: 'https://firebasestorage.googleapis.com/v0/b/test-8d0bf.appspot.com/o/images%2FS__5120002.jpg?alt=media&token=0d0a3277-931a-4822-a88c-9bed623c0c19'
+        image: 'https://firebasestorage.googleapis.com/v0/b/test-8d0bf.appspot.com/o/images%2Fdefault-card-image.jpg?alt=media&token=a5c8f131-a13b-48e2-9c47-eeddc6f41264'
+        # image: 'https://firebasestorage.googleapis.com/v0/b/test-8d0bf.appspot.com/o/images%2FS__5120002.jpg?alt=media&token=0d0a3277-931a-4822-a88c-9bed623c0c19'
       }
     end
 
